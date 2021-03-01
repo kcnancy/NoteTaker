70f3a024deee08d7bcd66cc84b0c86ff7dd882dc
Nancy McBride <njmcbride224@gmail.com>
2021-03-01 14:38:55 -0600

    adding routes and json files

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,39 @@
+const { fstat } = require("fs");
+const express = require("express");
+const path = require("path");
+
+app.get("/api/notes", function(req, res) {
+    res.json(notes);
+});
+
+app.post("/api/notes", function(req, res){
+    let newNote =req.body;
+    notes.push(newNote);
+    updateDb();
+    return console.log("added new note: "+newNote.title);
+});
+
+app.get("/api/notes/:id", function(req, res) {
+    res.json(notes[req.params.id]);
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+    notes.splice(req.params.id, 1);
+    updateDb();
+    console.log("deleted note"+params.id);
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+function updateDb() {
+    fs.writeFile("db/db.json",JSON.stringify(notes, "\t"), err=> {
+        if (err) throw err;
+        return true;
+    });
+}
